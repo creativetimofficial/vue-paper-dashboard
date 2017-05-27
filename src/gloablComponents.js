f4f2659f84863764d1257b6dd9c0d74602822ff6
cristijora <joracristi@gmail.com>
2017-05-27 22:01:30 +0300

    Initial dist subtree commit

--- src/gloablComponents.js
+++
@@ -1,9 +0,0 @@
-import fgInput from './components/UIComponents/Inputs/formGroupInput.vue'
-
-var GlobalComponents = {
-  install (Vue) {
-    Vue.component('fg-input', fgInput)
-  }
-}
-
-export default GlobalComponents
--- test/unit/helpers/mount.js
+++
@@ -1,6 +0,0 @@
-import Vue from "vue";
-window.mount = function mount(Component, propsData) {
-  const Ctor = Vue.extend(Component)
-  const vm = new Ctor({propsData}).$mount()
-  return vm;
-}
