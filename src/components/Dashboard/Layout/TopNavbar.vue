0b4d27baa76ce01e59741db655aeec29d6625c83
Tomas Pozo <tomaspozogarzon@gmail.com>
2017-07-07 19:23:27 -0500

    Replace v-click-outside with vue-clickaway

--- src/components/Dashboard/Layout/TopNavbar.vue
+++ src/components/Dashboard/Layout/TopNavbar.vue
@@ -25,6 +25,14 @@
                <li><a href="#">Notification 4</a></li>
                <li><a href="#">Another notification</a></li>
              </drop-down>
+
+             <drop-down title="5 Alerts" icon="ti-bell">
+               <li><a href="#">Notification 1</a></li>
+               <li><a href="#">Notification 2</a></li>
+               <li><a href="#">Notification 3</a></li>
+               <li><a href="#">Notification 4</a></li>
+               <li><a href="#">Another notification</a></li>
+             </drop-down>
           <li>
             <a href="#" class="btn-rotate">
               <i class="ti-settings"></i>
