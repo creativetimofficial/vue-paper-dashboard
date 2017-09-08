cf8041487ee1ebc4609bf0b50bbb9250e756b5c9
Cristi Jora <joracristi@gmail.com>
2017-09-08 10:51:33 +0300

    Update globalDirectives.js

--- src/globalDirectives.js
+++ src/globalDirectives.js
@@ -1,7 +1,7 @@
 import { directive as vClickOutside } from 'vue-clickaway'
 
 /**
- * You can register global components here and use them as a plugin in your main Vue instance
+ * You can register global directives here and use them as a plugin in your main Vue instance
  */
 
 const GlobalDirectives = {
