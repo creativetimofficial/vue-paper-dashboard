67089cbc21f7a9ddc30ba0792cb6d63fc0581d1a
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2017-09-20 10:47:10 +0900

    v-for should have key

--- src/components/UIComponents/SidebarPlugin/SideBar.vue
+++ src/components/UIComponents/SidebarPlugin/SideBar.vue
@@ -19,7 +19,7 @@
       </slot>
       <ul :class="navClasses">
         <!--By default vue-router adds an active class to each route link. This way the links are colored when clicked-->
-        <router-link v-for="(link,index) in sidebarLinks" :to="link.path" tag="li" :ref="link.name">
+        <router-link v-for="(link,index) in sidebarLinks" :to="link.path" tag="li" :ref="link.name" :key="index">
           <a>
             <i :class="link.icon"></i>
 
