0f97035fcde7ab337392e6c6f57471a1517869e5
cristij <joracristi@gmail.com>
2018-01-05 11:41:43 +0200

    Add card to global components

--- src/globalComponents.js
+++ src/globalComponents.js
@@ -1,14 +1,13 @@
-import fgInput from './components/UIComponents/Inputs/formGroupInput.vue'
-import DropDown from './components/UIComponents/Dropdown.vue'
-
+import {FormGroupInput, Card, DropDown} from './components/UIComponents/index'
 /**
  * You can register global components here and use them as a plugin in your main Vue instance
  */
 
 const GlobalComponents = {
   install (Vue) {
-    Vue.component('fg-input', fgInput)
+    Vue.component('fg-input', FormGroupInput)
     Vue.component('drop-down', DropDown)
+    Vue.component('card', Card)
   }
 }
 
