b18e3b19d65168da210687902694b73a7840ea50
cristij <joracristi@gmail.com>
2018-01-05 11:41:27 +0200

    Add Card component for easier migration of cards

---
+++ src/components/UIComponents/Cards/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <div class="card-image" v-if="$slots.image">
+      <slot name="image"></slot>
+    </div>
+    <div class="card-header" v-if="$slots.header || title">
+      <slot name="header">
+        <h4 class="card-title">{{title}}</h4>
+        <p class="card-category" v-if="subTitle">{{subTitle}}</p>
+      </slot>
+    </div>
+    <div class="card-body">
+      <slot></slot>
+    </div>
+    <div class="card-footer" v-if="$slots.footer">
+      <hr>
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'card',
+    props: {
+      title: String,
+      subTitle: String
+    }
+  }
+</script>
+<style>
+</style>
