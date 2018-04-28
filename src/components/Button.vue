a6dcb6f4b1f5d15ad36d80404b9e0cacf08c030e
cristij <joracristi@gmail.com>
2018-04-28 23:22:57 +0300

    Update for button, dropdown, fgInput and table

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,6 +2,7 @@
   <component
     :is="tag"
     :type="nativeType"
+    :disabled="disabled || loading"
     class="btn"
     :class="[
       {'btn-round': round},
@@ -11,8 +12,7 @@
       {[`btn-outline-${type}`]: type && outline},
       {[`btn-${size}`]: size},
       {'btn-link': simple}
-    ]"
-  >
+    ]">
     <slot name="loading">
       <i v-if="loading" class="fa fa-spinner fa-spin"></i>
     </slot>
@@ -21,6 +21,7 @@
 </template>
 <script>
 export default {
+  name: 'p-button',
   props: {
     tag: {
       type: String,
@@ -31,6 +32,7 @@ export default {
     outline: Boolean,
     block: Boolean,
     loading: Boolean,
+    disabled: Boolean,
     type: {
       type: String,
       default: "default"
