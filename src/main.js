3a4e1735dda05f336b0c365797a5287f75914417
cristij <joracristi@gmail.com>
2019-08-18 13:52:12 +0300

    Update some packages, add copyright comments

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/*!
+
+ =========================================================
+ * Vue Paper Dashboard - v2.0.0
+ =========================================================
+
+ * Product Page: http://www.creative-tim.com/product/paper-dashboard
+ * Copyright 2019 Creative Tim (http://www.creative-tim.com)
+ * Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard/blob/master/LICENSE.md)
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ */
 import Vue from "vue";
 import App from "./App";
 import router from "./router/index";
