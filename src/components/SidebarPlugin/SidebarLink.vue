fbbb665537e52cd064467b5673423b7422143cdc
Maisam Afshar <maisam.afshar@gmail.com>
2023-04-25 07:11:37 +0430

    fix deprecated-router-link-tag

--- src/components/SidebarPlugin/SidebarLink.vue
+++ src/components/SidebarPlugin/SidebarLink.vue
@@ -1,18 +1,17 @@
 <template>
-  <component
-    :is="tag"
-    @click.native="hideSidebar"
-    class="nav-item"
-    v-bind="$attrs"
-    tag="li"
-  >
-    <a class="nav-link">
+  <li class="nav-item">
+    <component
+      :is="tag"
+      class="nav-link"
+      @click.native="hideSidebar"
+      v-bind="$attrs"
+    >
       <slot>
         <i v-if="icon" :class="icon"></i>
         <p>{{ name }}</p>
       </slot>
-    </a>
-  </component>
+    </component>
+  </li>
 </template>
 <script>
 export default {
