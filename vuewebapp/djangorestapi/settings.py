db0eac9f8062e30054cda8a89d333b0bbc147646
stefanherdy <stefan.herdy@gmail.com>
2023-06-22 10:11:43 +0200

    ignoretest

--- vuewebapp/djangorestapi/settings.py
+++ vuewebapp/djangorestapi/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret! (This is an old secret key)
 #SECRET_KEY = 'o_b*cq5*3^+6$jhhvy9jo=&&ogzg8bm4i-yidc4fp0t9)^luqp'
-SECRET_KEY = '^966tpilskc7)9t%*$b8**5dm_o@^9j#ak*+pu-(nz7h08xzo6'
+SECRET_KEY = '0gmvb0qpd2jwaz985g%!5zg))v)4y2(gln_)$)(8crmwg+jwdi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
